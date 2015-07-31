c06ae07e50d1e7d94cbab10c72c1d499f810384e
Frank Wang <fkwang@uchicago.edu>
2015-07-31 12:20:21 -0500

    Applicative parsing

--- image_generator.hs
+++ image_generator.hs
@@ -1,157 +1,94 @@
-module Main where
-
-import System.Environment
+import PPM
 import System.IO
-import Data.Char
 import Control.Applicative as A
 import Control.Monad.ST
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as M
 import qualified Data.ByteString.Char8 as B
-import qualified Data.Attoparsec.ByteString.Char8 as  P
-import qualified Text.ParserCombinators.ReadP as R
+import qualified Data.ByteString.Lazy.Char8 as LB
+import qualified Data.Attoparsec.ByteString.Char8 as P
+import qualified Data.Attoparsec.ByteString.Lazy as LP
 
-import PPM
-import Data.Either
 
------ PARSING -----
--------------------
+apply :: a -> (a -> b) -> b
+apply a = \f -> f a
+
+
+---------- PARSING ----------
+
+iT = B.pack "*I"
+
 data Complex = Complex Double Double
 	deriving (Show)
 
-complexSum :: Complex -> Complex -> Complex
-complexSum (Complex a b) ( Complex c d) = Complex (a + c) (b + d)
+cAdd:: Complex -> [Complex] -> Complex
+cAdd (Complex a b) [(Complex c d)] = Complex (a + c) (b + d)
+cAdd c _ = c
+cConv :: Complex -> Complex
+cConv (Complex a b) = Complex b a
 
-readComplex :: B.ByteString -> Either String Complex
-readComplex = P.parseOnly parseComplex
+parseTerm :: P.Parser Complex
+parseTerm = parseDouble <*> parseConv
+	where 
+		parseDouble = liftA (\x -> apply (Complex x 0)) $
+			liftA2 (\x y -> read ((x:y) ++ "0")) P.anyChar $ many $ P.satisfy $ \x -> P.isDigit x || x == '.'
+		parseConv = P.option id $ liftA (const cConv) $ P.string $ B.pack "*I"
 
--- Tokens
-xT = B.pack "x->"
-iT = B.pack "*I"
-
--- Parsers
-parseX :: P.Parser B.ByteString
-parseX = P.string xT
-
-parseI :: P.Parser Bool
-parseI = P.option False $ fmap (const True) $ P.string iT
-
-parseNeg :: P.Parser Bool
-parseNeg = P.option False $ fmap (const True) $ P.char '-'
-
-parseAdd :: P.Parser Bool
-parseAdd = P.option False $ fmap (const True) $ P.char '+'
-
-parseSingle :: P.Parser Complex
-parseSingle = P.option (Complex 0 0) $ do
-	digits <- P.double
-	i <- parseI
-	if i
-		then return $ Complex 0 digits
-	else return $ Complex digits 0
+test :: P.Parser String
+test = liftA2 (\x y -> (x:y)) P.anyChar $ many $ P.satisfy $ \x -> P.isDigit x || x == '.'
 
 parseComplex :: P.Parser Complex
-parseComplex = do
-	a <- parseSingle
-	b <- parseSingle
-	return $ complexSum a b
+parseComplex = liftA2 cAdd parseTerm $ many $ many (P.char '+') A.*> parseTerm
 
 
------ DATA FORMATTING -----
----------------------------
-remSpace :: B.ByteString -> B.ByteString
-remSpace = B.filter (\x -> x /= ' ' && x /= '\n')
+---------- IMAGE PROCESSING ----------
 
-split :: B.ByteString -> [B.ByteString]
-split = B.splitWith (\x -> x == '{' || x == '}' || x == ',')
+bounds = 2.0 :: Double
+imgSize = 400 :: Int
 
-remTrash :: [B.ByteString] -> [B.ByteString]
-remTrash = filter $ B.notElem 'x'
+intensity = 300 :: Int
+falloff = 0.8 :: Double
+(r, g, b) = (1.0, 0.3, 0.1)
 
-parseAll :: [B.ByteString] -> [Either String Complex]
-parseAll = map readComplex
+computeIndex :: Double -> Int
+computeIndex x = (+) 1 $ floor $ (x + bounds) / (2 * bounds / fromIntegral imgSize)
 
-clean :: [Either String Complex] -> [Complex]
-clean [] = []
-clean ((Right x) : xs) = x : clean xs
-clean ((Left x) : xs) = clean xs
+convCoord ::(Int, Int) -> Int
+convCoord (h, v) = v * imgSize + h
 
+computeCIndex :: Complex -> Int
+computeCIndex (Complex r i) = convCoord (computeIndex r, computeIndex i)
 
------ IMAGE PROCESSING -----
-----------------------------
-left = (-2.0)
-right = 2.0
-top = 2.0
-bottom = (-2.0)
-<<<<<<< HEAD
-vertPix = 800 -- Pixel ratios must adhere to coordinate ratios
-horzPix = 800
-=======
-vertPix = 6000 -- Pixel ratios must adhere to coordinate ratios
-horzPix = 6000
->>>>>>> parent of c9a74a7... Fixed indexing bug
-
-convCoord :: (Int, Int) -> Int
-convCoord (h, v) = v * horzPix + h
-
-computePixel :: Double -> Double -> Double -> Int -> Int
-computePixel leftDown rightUp cord nPix
-	| cord < leftDown || cord > rightUp = (-1)
-	| otherwise = floor $ (cord - leftDown) / pixLength
-		where pixLength = (rightUp - leftDown) / fromIntegral nPix 
-
-convComplex :: Complex -> (Int, Int)
-convComplex (Complex r i) = (h, v)
-	where
-		h = computePixel bottom top r vertPix
-		v = computePixel left right i horzPix
-
-incrTup :: (Int, Int, Int) -> Int -> (Int, Int, Int)
-incrTup (a, b, c) i = (a + i, b + i, c + i)
-
-incr mv i = do
-	item <- M.read mv i
-	if item == (10,10,10)
-		then return ()
-	else M.write mv i $ incrTup item 1
-
-genVec :: [Complex] -> V.Vector (Int, Int, Int)
+genVec :: [Complex] -> V.Vector Int
 genVec xs = runST $ do
-	mv <- M.replicate (vertPix * horzPix) (0, 0, 0)
-	mapM_ (incr mv) $ map (convCoord . convComplex) xs
+	mv <- M.replicate (imgSize ^ 2) 0
+	mapM_ (incr mv) $ map computeCIndex xs -- unsafeModify
 	V.freeze mv
+		where
+			incr mv i = do
+				pre <- M.unsafeRead mv i
+				M.unsafeWrite mv i $ pre + 1
 
-genImage :: V.Vector (Int, Int, Int) -> PPM
-genImage v = PPM v vertPix horzPix $ (\(x, y, z) -> x) $ V.maximum v
+colorFunction :: Int -> (Int, Int, Int)
+colorFunction n = (floor (y * r), floor(y * g), floor(y * b))
+	where y = ((fromIntegral (intensity * n)) ** falloff)
+
+colorFunction1 n = (n, n, n)
+
+genImage :: V.Vector Int -> PPM
+genImage v = PPM (V.map colorFunction1 v) imgSize imgSize $ V.maximum v
 
 
------ TEST CODE -----
----------------------
-count :: (a -> Bool) -> [a] -> Int
-count _ [] = 0
-count f (x:xs)
-	| f x = 1 + count f xs
-	| otherwise = count f xs
+extr :: LP.Result a -> a
+extr (LP.Done _ r) = r
+extr (LP.Fail _ _ y) = error y
 
-isRight :: Either a b -> Bool
-isRight (Left _) = False
-isRight (Right _) = True	
+---------- MAIN ----------
 
-center = [Complex 0 0]
-
------ MAIN -----
-----------------
 main = do
-	-- Import / Parser
-	rawData <- B.readFile "data"
+	rawData <- liftA LB.words (LB.readFile "/mnt/hgfs/outputs/out_1-14.txt")
+	let formatedData = map (extr.LP.parse parseComplex) rawData
 
-	let formatedData = p ++ p -- Doubled bc/ new data contains half the points
-		where p = clean $ parseAll $ remTrash $ split $ remSpace rawData
-
-	-- Create Image
-	let ppm = genImage $ genVec $ formatedData
-
-	-- Write Image
 	h <- openFile "test.ppm" WriteMode
-	writeImage ppm h
+	writeImage (genImage (genVec formatedData)) h
 	hClose h
