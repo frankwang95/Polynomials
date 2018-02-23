3dc119188aeaaad18942951992aee46e1364344c
frankwang95 <fkwang@uchicago.edu>
2018-02-22 23:13:26 -0800

    Better image parsing logic

--- generate_image.py
+++ generate_image.py
@@ -3,10 +3,11 @@ import numpy as np
 from PIL import Image
 
 
-n = 1860000
-filename = 'out.txt'
+n = 5000000
+input_filename = 'out.txt'
 image_range = np.array([[-2.0, 2.0], [-2.0, 2.0]])
-pixels = 1024
+pixels = 15000
+output_filename = 'image.tif'
 
 
 def generate_hist(data):
@@ -18,30 +19,28 @@ def generate_hist(data):
 def parse_to_numbers(chunk):
     roots = []
     for c in chunk:
-        if ' ' not in c:
-            roots.append((float(c), 0))
-        else:
-            parts = c.split()
-            r = float(parts[0])
-            i = float(''.join(parts[1:])[:-1])
-            roots.append((r, i))
+        roots.append([float(s) for s in c.split(',')])
     return np.array(roots)
 
-
 def main():
-    with open(filename, 'r') as f:
+    with open(input_filename, 'r') as f:
         histogram = np.zeros((pixels, pixels))
+        i = 0
         while True:
+            print('\rdone: {}'.format(i * n), end='')
             chunk = list(islice(f, n))
             if not chunk: break
             data = parse_to_numbers(chunk)
             histogram += generate_hist(data)
+            i += 1
+    print('\n')
+    print('Finished generating historam, writing image...')
 
     histogram = histogram * 4
     histogram = np.clip(np.log(histogram + 1) * 2 / np.log(histogram.max() + 1), 0, 1)
     histogram = (np.stack((histogram, histogram, histogram), axis=2) * 255).astype('uint8')
     im = Image.fromarray(histogram)
-    im.save('image.tif')
+    im.save(output_filename)
 
 
 main()
